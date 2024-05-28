59e345d38a0328f02d02010a81505c62bb528228
FilippoScalabrin <164555381+FilippoScalabrin@users.noreply.github.com>
2024-05-28 07:34:52 +0200

    Add files via upload

--- regole_associative.R
+++ regole_associative.R
@@ -1,25 +1,44 @@
 library(arules)
+library(arulesViz)
 library(dplyr)
 
-cars <- read.csv(file.choose())
-optional <- cars[, 33:76]
-optional[, colnames(optional)] <- lapply(optional[, colnames(optional)], factor)
+setwd("C:/Users/pippo/OneDrive/Documenti/strumenti-statistici-dati-aziendali/autoscout24")
+cars <- read.csv("cars-clean-v2-imputed.csv", stringsAsFactors = TRUE)
+optional <- cars[, 32:76]
+str(optional)
+# Lettura dei dati sugli optional.
 
+optional[, colnames(optional)] <- lapply(optional[, colnames(optional)], 
+                                         factor)
 optional <- as(optional, "transactions")
+# La matrice con gli optional viene resa come un oggetto della
+# classe "transactions".
 
 f.itemset <- apriori(optional, 
                      parameter = list(supp = 0.5, conf = 0.9, 
-                                      target = "frequent itemset"))
+                                      target = "rules"))
+inspect(f.itemset)
+# Ricerca di regole con supporto maggiore di 0.5 e fiducia
+# maggiore di 0.9.
 
-summary(f.itemset)
+inspect(sort(subset(f.itemset, subset = lift > 1), by = "confidence"))
+# Si selezionano solo le regole con lift superiore a 1
+# e le si ordinano per fiducia decrescente.
 
-inspect(f.itemset[1:10])
+f.itemset <- apriori(optional, 
+                     parameter = list(supp = 0.5, conf = 0.7, 
+                                      target = "rules"))
+inspect(f.itemset)
+# Ricerca di regole con supporto maggiore di 0.5 e fiducia
+# maggiore di 0.9.
 
-rules <- apriori(optional,
-                 parameter = list(support = 0.5, confidence = 0.9,
-                                  target = "rules"))
+inspect(sort(subset(f.itemset, subset = lift > 1.2), by = "confidence"))
+# Si selezionano solo le regole con lift superiore a 1
+# e le si ordinano per fiducia decrescente.
 
-summary(rules)
+important_rules <- subset(f.itemset, subset = lift > 3)
+plot(important_rules)
 
-important_rules <- subset(rules, subset = lift > 1)
-inspect(important_rules)
+plot(important_rules, method = "graph")
+plot(important_rules, method = "graph", engine = "interactive")
+plot(important_rules, method = "graph", engine = "visNetwork")
